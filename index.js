e632afcd0d7a657a98761994ec3bcf63f4b23a0f
alberttien <alberttien@aol.com>
2018-08-31 10:50:28 -0400

    Update index.js

--- index.js
+++ index.js
@@ -44,21 +44,25 @@ $(function() {
     // and a device ID - for browser-based apps, we'll always just use the 
     // value "browser"
     
-    $.getJSON('createChatAccessToken.php', {
-        device: 'browser'
-    }, function(data) {
-        // Alert the user they have been assigned a random username
-        alert(data);
-        username = data.identity;
-        print('You have been assigned a random username of: ' 
-            + '<span class="me">' + username + '</span>', true);
+    //$.getJSON('createChatAccessToken.php', {
+    //    device: 'browser'
+    //}, function(data) {
+    //    // Alert the user they have been assigned a random username
+    //    alert(data);
+    //    username = data.identity;
+    //    print('You have been assigned a random username of: ' 
+    //        + '<span class="me">' + username + '</span>', true);
 
         // Initialize the Chat client
-        chatClient = new Twilio.Chat.Client(data.token);
-        chatClient.getSubscribedChannels().then(createOrJoinGeneralChannel);        
-    }).fail(function() {
-    console.log( "error" )});
+    //    chatClient = new Twilio.Chat.Client(data.token);
+    //    chatClient.getSubscribedChannels().then(createOrJoinGeneralChannel);        
+    //}).fail(function() {
+    //console.log( "error" )});
 
+    var token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiIsImN0eSI6InR3aWxpby1mcGE7dj0xIn0.eyJqdGkiOiJTSzgyYTVmMjRkNTJlYTM3NWI5NGNkMzJlYmJlMGY1NzViLTE1MzU3MjY5NTIiLCJpc3MiOiJTSzgyYTVmMjRkNTJlYTM3NWI5NGNkMzJlYmJlMGY1NzViIiwic3ViIjoiQUMzY2ZlZTIxMzJiODAyNmFiMjhjNDQ3MWFjM2VjMTE1YSIsImV4cCI6MTUzNTczMDU1MiwiZ3JhbnRzIjp7ImlkZW50aXR5Ijoiam9obl9kb2UiLCJjaGF0Ijp7InNlcnZpY2Vfc2lkIjoiSVMxMTU2NzdkNWUzZGI0YTE0ODJkYzYzMzdlYmQwNWNlOSJ9fX0.eq_D0UjtwsL6b0BsID2aPn3BUQaRacI1YKZgS_f2lHI";
+	chatClient = new Twilio.Chat.Client.create(token);
+    chatClient.getSubscribedChannels().then(createOrJoinGeneralChannel); 
+    
     function createOrJoinGeneralChannel() {
         // Get the general chat channel, which is where all the messages are
         // sent in this simple application
