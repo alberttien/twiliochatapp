4bc7fc0bb8bd9b1cfd2b638572722d60dbb9f8d3
alberttien <alberttien@aol.com>
2018-08-31 11:52:43 -0400

    Update index.js

--- index.js
+++ index.js
@@ -44,16 +44,21 @@ $(function() {
 
     
     var token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiIsImN0eSI6InR3aWxpby1mcGE7dj0xIn0.eyJqdGkiOiJTSzgyYTVmMjRkNTJlYTM3NWI5NGNkMzJlYmJlMGY1NzViLTE1MzU3MzAyNzUiLCJpc3MiOiJTSzgyYTVmMjRkNTJlYTM3NWI5NGNkMzJlYmJlMGY1NzViIiwic3ViIjoiQUMzY2ZlZTIxMzJiODAyNmFiMjhjNDQ3MWFjM2VjMTE1YSIsImV4cCI6MTUzNTczMzg3NSwiZ3JhbnRzIjp7ImlkZW50aXR5Ijoiam9obl9kb2UiLCJjaGF0Ijp7InNlcnZpY2Vfc2lkIjoiSVMxMTU2NzdkNWUzZGI0YTE0ODJkYzYzMzdlYmQwNWNlOSJ9fX0.Pr9bErMQKbKa2tFeReRe7V2LF4LW-ZT1w2IJBJV38nU";
-    chatClient = Twilio.Chat.Client.create(token);
-    console.log('Creating general channel');
-    chatClient.createChannel({
-        uniqueName: 'general',
-        friendlyName: 'General Chat Channel'
-    }).then(function(channel) {
-        console.log('Created general channel:');
-        console.log(channel);
-        generalChannel = channel;
-        //setupChannel();
+    
+    var chatClient = Twilio.Chat.Client.create(token).then(client => {
+        alert('test');
     });
+    
+//    chatClient = Twilio.Chat.Client.create(token);
+//    console.log('Creating general channel');
+//    chatClient.createChannel({
+//        uniqueName: 'general',
+//        friendlyName: 'General Chat Channel'
+//    }).then(function(channel) {
+//        console.log('Created general channel:');
+//        console.log(channel);
+//        generalChannel = channel;
+        //setupChannel();
+//    });
 
 });
